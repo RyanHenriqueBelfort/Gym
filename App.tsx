a21c374ff5239e6fde3285359c72b98890fc256a
Ryan Henrique de Assis Belfort <ryanhenriqueab@gmail.com>
2023-06-23 10:45:34 -0300

    Feat: ✨ First commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NativeBaseProvider } from 'native-base'
+
+
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
 export default function App() {
+  const fontsLoaded = useFonts({Roboto_400Regular, Roboto_700Bold})
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <StatusBar backgroundColor='transparent' translucent barStyle={'light-content'}/>
+      {fontsLoaded ? 
+        <Text>Open up App.tsx to start working on your app!</Text>
+        : 
+        ''
+      }
+    </NativeBaseProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
