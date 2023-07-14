993df561b521a12548a704bac083ec027dd7912f
Ryan Henrique de Assis Belfort <ryanhenriqueab@gmail.com>
2023-07-14 09:56:32 -0300

    Feat: ✨Criando componentes ExercisesCard e History

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,9 +1,34 @@
-import { Center, Text } from 'native-base';
+import { useState } from "react";
+import { HistoryCard } from "@components/HistoryCard";
+import { ScreenHeader } from "@components/ScreenHeader";
+import { Heading, VStack, SectionList } from "native-base";
 export function History() {
+  const [exercises, setExercises] = useState([
+    {
+      title: "14.07.23",
+      data: ["Puxada frontal", "Remada unilateral"],
+    },
+    {
+      title: "15.07.23",
+      data: ["Puxada frontal"],
+    },
+  ]);
+
   return (
-    <Center flex={1}>
-      <Text>History</Text>
-    </Center>
-  )
-  }
- 
+    <VStack flex={1}>
+      <ScreenHeader title="Histórico de exercícios" />
+
+      <SectionList
+        sections={exercises}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <HistoryCard />}
+        renderSectionHeader={({ section }) => (
+          <Heading color="gray.200" fontSize="md" mt={10} mb={3}>
+            {section.title}
+          </Heading>
+        )}
+        px={8}
+      />
+    </VStack>
+  );
+}
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,14 +1,53 @@
-import { Groups } from '@components/Groups';
-import { HomeHeader } from '@components/HomeHeader';
-import { Center, VStack } from 'native-base';
+import { ExerciseCard } from "@components/ExerciseCard";
+import { Groups } from "@components/Groups";
+import { HomeHeader } from "@components/HomeHeader";
+import { HStack, VStack, FlatList, Heading, Text } from "native-base";
+import { useState } from "react";
 export function Home() {
+  const [exercices, setExercices] = useState(["Puxada fronta", "Remada curvada", "Remada unilateral", "Levantamento terra"]);
+  const [group, setGroup] = useState(["Costas", "Perna", "Biceps", "ombro"]);
+  const [groupSelected, setGroupSelected] = useState("costas");
+
   return (
     <VStack flex={1}>
       <HomeHeader />
-      <Groups 
-       name='Teste'
+      <FlatList
+        data={group}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Groups
+            name={item}
+            isActive={groupSelected.toUpperCase() === item.toUpperCase()}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{ px: 8 }}
+        my={10}
+        maxH={10}
       />
+
+      <VStack flex={1} px={8}>
+        <HStack justifyContent="space-between" mb={5}>
+          <Heading color="gray.200" fontSize="md">
+            Exercícios
+          </Heading>
+
+          <Text color="gray.200" fontSize="sm">
+            {exercices.length}
+          </Text>
+        </HStack>
+        <FlatList 
+          data={exercices}
+          keyExtractor={item => item}
+          renderItem={({item}) => (
+            <ExerciseCard />
+          )}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{paddingBottom: 20}}
+        />
+      </VStack>
     </VStack>
-  )
-  }
- 
+  );
+}
